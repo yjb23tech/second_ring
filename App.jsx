887e56f6c0f1a86b756a222484d0c38f542889c4
yjb23tech <yjb23tech@gmail.com>
2024-01-09 16:43:26 +0000

    The power of the Dev server

--- App.jsx
+++ App.jsx
@@ -9,21 +9,21 @@ const App = (props) => {
                 itunes_song_cover_art = "track_8_song_art.png"
                 itunes_song_title = "RunWay Remix"
                 itunes_album_title = "My Beautiful Dark Twisted Fantasy"
-                itunes_artist_title = "Kanye West"
+                itunes_artist_title = "Kanye West aka Yeezy"
                 itunes_genre_title = "Baroque Sophisticated Ignorance"
             />
             <Card 
                 itunes_song_cover_art = "track_9_song_art.png"
-                itunes_song_title = "Dark Fantasy"
+                itunes_song_title = "Dark Fantasy Remix"
                 itunes_album_title = "My Beautiful Dark Twisted Fantasy"
                 itunes_artist_title = "Kanye West"
                 itunes_genre_title = "Baroque Sophisticated Ignorance"
             />
             <Card 
                 itunes_song_cover_art = "track_10_song_art.png"
-                itunes_song_title = "Hell Of A Life"
+                itunes_song_title = "Hell Of A Life Remix"
                 itunes_album_title = "My Beautiful Dark Twisted Fantasy"
-                itunes_artist_title = "Kanye West"
+                itunes_artist_title = "Kanye West aka Mike Jack"
                 itunes_genre_title = "Baroque Sophisticated Ignorance"
             />
         </div>
