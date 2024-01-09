49b42a52d6a158afbc21d6ec135b5e21b831c1f5
yjb23tech <yjb23tech@gmail.com>
2024-01-09 12:02:53 +0000

    Keenan Keyman

---
+++ App.jsx
@@ -0,0 +1,12 @@
+const App = (props) => {
+    return (
+        <div>
+            <h1>Cold Hearted.</h1>
+        </div>
+    )
+}
+
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(<App />)
+
